5edc71bd2830a8029c4b248e7844bb5cb6d2d471
IrinZh <irishinskin@gmail.com>
2019-04-19 14:11:47 +0300

    поправила на числовые значения переменной cost, переименовала в .jshintrc

--- Lesson1/task_1/script.js
+++ Lesson1/task_1/script.js
@@ -13,15 +13,16 @@ let  appData = {
 };
 //вариант с двумя вопросами по два раза
 let objectOfExpenditure = prompt("Введите обязательную статью расходов в этом месяце", "первая обязательная статья расходов"),
-    cost = prompt("Во сколько обойдется?", "");
+    cost = +prompt("Во сколько обойдется?", "");
     appData.expenses[objectOfExpenditure] = cost; 
     objectOfExpenditure = prompt("Введите обязательную статью расходов в этом месяце", "вторая обязательная статья расходов"),
-    cost = prompt("Во сколько обойдется?", "");
+    cost = +prompt("Во сколько обойдется?", "");
     appData.expenses[objectOfExpenditure] = cost;
 
 console.log("бюджет на 1 день "+ +money/30 + " руб.");
 
 console.log(appData); 
-// проверяем как записался в объект объект expenses с данными статьи расходов и стоимости
+// проверяем как записался в объект объект expenses с данными 
+//статьи расходов и стоимости
 // console.log(typeof(objectOfExpenditure));
 // статья расходов выводится без "", но все равно строка
--- Lesson1/task_2/tempCodeRunnerFile.js
+++ Lesson1/task_2/tempCodeRunnerFile.js
@@ -1,5 +1,4 @@
-'use strict';
-let num = 33721,
+let num = 33721;
 // arr = [...num+''].map(n=>+n);
 arr = [...num+''];
 
@@ -8,9 +7,9 @@ let result = arr.reduce(function(a, b) {
 });
 
 console.log("произведение цифр числа " + num + ": " + result);
-let res3 = result**3,
-    arrRes3 = [...res3+''],
-    n = 2;
+let res3 = result**3;
+    arrRes3 = [...res3+''];
+    n = 2;    
 console.log("первые две цифры числа " + result +", возведенного в 3 степень: " + arrRes3.slice(0, n));
 
 
